9ab3f2e409d4d37d8f149ccf9fd9c4192919714c
Brandon-Badenhorst <ST10470965@vcconnect.edu.za>
2025-03-04 11:39:22 +0200

    Added prompt
    Added println to learn how edited code saves in github

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -12,5 +12,6 @@ public class PrjClasses {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("I just did mt first GitHub activity!");
     }
 }
